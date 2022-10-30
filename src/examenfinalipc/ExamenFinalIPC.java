9db9db97ad621848f3ea8bbdfc261c163bd37f34
LuisCastellanos <luisda.betancourt@gmail.com>
2022-10-29 21:57:57 -0600

    anadiendo pdf

--- src/examenfinalipc/ExamenFinalIPC.java
+++ src/examenfinalipc/ExamenFinalIPC.java
@@ -11,20 +11,28 @@ import java.util.Scanner;
  * @author Luisda
  */
 public class ExamenFinalIPC {
-    
-    public static void ImprimePiramide(int NumFilas){
-        
+
+    public static void ImprimePiramide(int NumFilas) {
+
         for (int i = 0; i < NumFilas; i++) {
-            for (int j = 0; j < (NumFilas-i); j++) {
+            for (int j = 0; j < (NumFilas - i); j++) {
                 System.out.print(" ");
-               
+
             }
-            
-            for (int j = 0; j < 2*i+1; j++) {
+
+            for (int j = 0; j < 2 * i + 1; j++) {
                 System.out.print("*");
             }
             System.out.println("");
         }
+    }
+
+    public static int MaximizadorLeche(int PesoVacas[], int produccionLeche[], int NumVacas, int pesoCamion) {
+        int LecheMaxima = 0;
+        int peso = 0;
+        boolean calculando = true;
+
+        return LecheMaxima;
     }
 
     /**
@@ -38,15 +46,20 @@ public class ExamenFinalIPC {
         int opcion;
         int num1;
         int num2;
-        int numPiramide=0;
-        int contador=1;
-        int numImpar=1;
-        
+        int numPiramide = 0;
+        int contador = 1;
+        int numImpar = 1;
+
+        //Variables vacas
+        int numVacas = 6;
+        int pesoCamion = 700;
+        int pesoVacas[] = {360, 250, 400, 180, 50, 90};
+        int produccionLeche[] = {40, 35, 43, 28, 12, 13};
 
         Scanner sc = new Scanner(System.in);
 
         while (salir) {
-            System.out.println("Eliga el numero de la opcion: \n \n 1. Ejercicio 1 (Comparacion de numeros)  \n 2. Ejercicio 2 (Creacion de piramides)  \n 3.  \n 4. Salir");
+            System.out.println("Eliga el numero de la opcion: \n \n 1. Ejercicio 1 (Comparacion de numeros)  \n 2. Ejercicio 2 (Creacion de piramides)  \n 3. Ejercicio 3 (Piramide)  \n 4. Salir");
             opcion = sc.nextInt();
 
             switch (opcion) {
@@ -80,24 +93,32 @@ public class ExamenFinalIPC {
                             System.out.println("Presione enter para continuar");
                             sc.nextLine();
                             sc.nextLine();
-                        }
-                        else{
-                            impar=false;
+                        } else {
+                            impar = false;
                         }
                     }
-                    impar=true;
-                    
-                    while(numImpar!=numPiramide){
-                        numImpar=numImpar+2;
+                    impar = true;
+
+                    while (numImpar != numPiramide) {
+                        numImpar = numImpar + 2;
                         contador++;
                     }
                     ImprimePiramide(contador);
-                    
-                    
+                    numPiramide = 0;
+                    contador = 1;
+                    numImpar = 1;
+                    System.out.println("Presione enter para continuar");
+                    sc.nextLine();
+                    sc.nextLine();
 
                     break;
                 case (3):
-                    System.out.println("Opcion 3");
+                    System.out.println("xd");
+                    System.out.println("");
+                    System.out.println("Presione enter para continuar");
+                    sc.nextLine();
+                    sc.nextLine();
+
                     break;
 
             }
